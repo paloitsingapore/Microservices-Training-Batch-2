79c894b0c9021e2bed7ea8018a9c33a3d8cd6da4
zeagord <rsganesan@palo-it.com>
2019-09-24 13:36:05 +0800

    Adds streams

--- bootiful-hello/src/main/java/com/ntuc/income/userservice/BootifulHelloApplication.java
+++ bootiful-hello/src/main/java/com/ntuc/income/userservice/BootifulHelloApplication.java
@@ -2,12 +2,20 @@ package com.ntuc.income.userservice;
 
 import com.ntuc.income.userservice.entity.User;
 import com.ntuc.income.userservice.repository.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.cloud.stream.messaging.Sink;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,8 +25,11 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @RestController
 @EnableDiscoveryClient
+@EnableBinding(Sink.class)
+
 public class BootifulHelloApplication implements ApplicationRunner {
 
+	private static final Logger log = LoggerFactory.getLogger(BootifulHelloApplication.class);
 	@Autowired UserRepository userRepository;
 
 	public static void main(String[] args) {
@@ -40,6 +51,14 @@ public class BootifulHelloApplication implements ApplicationRunner {
 		userRepository.save(new User("Doe", 43));
 		userRepository.save(new User("Jill", 32));
 
+	}
+
+	@StreamListener(Sink.INPUT)
+	public void listen(@Payload String event,
+			@Header(KafkaHeaders.RECEIVED_TOPIC) String topic,
+			@Header(KafkaHeaders.RECEIVED_PARTITION_ID) Integer partition,
+			@Header(KafkaHeaders.OFFSET) Long offset){
+		log.info(event);
 	}
 
 
--- order-service/src/main/java/com/ntuc/income/orderservice/OrderServiceApplication.java
+++ order-service/src/main/java/com/ntuc/income/orderservice/OrderServiceApplication.java
@@ -1,18 +1,42 @@
 package com.ntuc.income.orderservice;
 
+import java.util.UUID;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.messaging.Source;
+import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.MessageBuilder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
-
+@EnableBinding(Source.class)
+@RestController
 public class OrderServiceApplication {
+
+  @Autowired Source source;
 
   public static void main(String[] args) {
     SpringApplication.run(OrderServiceApplication.class, args);
+  }
+
+  @GetMapping("/event")
+  public ResponseEntity orderEvent(){
+
+    Message<String> event = MessageBuilder
+        .withPayload(UUID
+            .randomUUID()
+            .toString())
+        .build();
+    source.output().send(event);
+    return ResponseEntity.accepted().build();
   }
 
 
